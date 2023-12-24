3b01b40da683aa014015d4d341bb91826e8470de
Othmaneamrani <othmaneamrani31@gmail.com>
2023-12-24 16:59:16 +0100

    bars

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
 export default function App() {
   return (
