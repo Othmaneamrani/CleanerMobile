6bd75a494afc85b97a0dbb396f697469ef01782d
Othmaneamrani <othmaneamrani31@gmail.com>
2023-12-29 02:44:24 +0100

    access

--- Components/Second.js
+++ Components/Second.js
@@ -1,14 +1,37 @@
-import { StyleSheet,Text, View } from "react-native";
+import { StyleSheet,Button, Image,View } from "react-native";
 import FooterSecond from "./FooterSecond";
 import NavbarSecond from "./NavbarSecond";
+import * as ImagePicker from 'expo-image-picker';
+import React, { useState } from 'react';
 
 export default function Second () {
+
+    const [imageSource, setImageSource] = useState(null);
+
+    const selectImage = async () => {
+        try {
+          const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            quality: 1,
+          });
+    
+          if (!result.cancelled) {
+            if (result.assets && result.assets.length > 0) {
+                setImageSource({ uri: result.assets[0].uri });
+              }          }
+        } catch (error) {
+          console.error('Error picking an image', error);
+        }
+      };
+    
+
     return (
         <View style={styles.container}>
         <NavbarSecond />
         <View style={styles.content}>
-            <Text>text</Text>
-        </View>
+        {imageSource && <Image source={imageSource}style={{ flex: 1, width: '100%', resizeMode: 'contain'}} />}
+      <Button title="Select Image" onPress={selectImage} />
+      </View>
         <FooterSecond />
         </View>
     )
